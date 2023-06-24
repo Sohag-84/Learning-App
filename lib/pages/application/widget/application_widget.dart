79ef2ad9596c1e018c05f597d6e6667f00e9e9db
Sohag <ihsohag-84@gmail.com>
2023-06-24 18:46:05 +0600

    seperate bottom navbar from application page

--- lib/pages/application/widget/application_widget.dart
+++ lib/pages/application/widget/application_widget.dart
@@ -1,14 +1,109 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:learning_app/common/values/colors.dart';
 
-Widget buildPage({required int index}){
+Widget buildPage({required int index}) {
   List<Widget> _widget = [
-    Center(child: Text("Home Page"),),
-    Center(child: Text("Search Page"),),
-    Center(child: Text("Course Page"),),
-    Center(child: Text("Chat Page"),),
-    Center(child: Text("Profile Page"),),
+    Center(
+      child: Text("Home Page"),
+    ),
+    Center(
+      child: Text("Search Page"),
+    ),
+    Center(
+      child: Text("Course Page"),
+    ),
+    Center(
+      child: Text("Chat Page"),
+    ),
+    Center(
+      child: Text("Profile Page"),
+    ),
   ];
   return _widget[index];
 }
+
+var bottomTabs = [
+  BottomNavigationBarItem(
+    label: "Home",
+    icon: SizedBox(
+      height: 15.h,
+      width: 15.w,
+      child: Image.asset("assets/icons/home.png"),
+    ),
+    activeIcon: SizedBox(
+      height: 15.h,
+      width: 15.w,
+      child: Image.asset(
+        "assets/icons/home.png",
+        color: AppColors.primaryElement,
+      ),
+    ),
+  ),
+  BottomNavigationBarItem(
+    label: "Search",
+    icon: SizedBox(
+      height: 15.h,
+      width: 15.w,
+      child: Image.asset("assets/icons/search.png"),
+    ),
+    activeIcon: SizedBox(
+      height: 15.h,
+      width: 15.w,
+      child: Image.asset(
+        "assets/icons/search.png",
+        color: AppColors.primaryElement,
+      ),
+    ),
+  ),
+  BottomNavigationBarItem(
+    label: "Course",
+    icon: SizedBox(
+      height: 15.h,
+      width: 15.w,
+      child: Image.asset("assets/icons/play-circle1.png"),
+    ),
+    activeIcon: SizedBox(
+      height: 15.h,
+      width: 15.w,
+      child: Image.asset(
+        "assets/icons/play-circle1.png",
+        color: AppColors.primaryElement,
+      ),
+    ),
+  ),
+  BottomNavigationBarItem(
+    label: "Chat",
+    icon: SizedBox(
+      height: 15.h,
+      width: 15.w,
+      child: Image.asset("assets/icons/message-circle.png"),
+    ),
+    activeIcon: SizedBox(
+      height: 15.h,
+      width: 15.w,
+      child: Image.asset(
+        "assets/icons/message-circle.png",
+        color: AppColors.primaryElement,
+      ),
+    ),
+  ),
+  BottomNavigationBarItem(
+    label: "Profile",
+    icon: SizedBox(
+      height: 15.h,
+      width: 15.w,
+      child: Image.asset("assets/icons/person2.png"),
+    ),
+    activeIcon: SizedBox(
+      height: 15.h,
+      width: 15.w,
+      child: Image.asset(
+        "assets/icons/person.png",
+        color: AppColors.primaryElement,
+      ),
+    ),
+  ),
+];
